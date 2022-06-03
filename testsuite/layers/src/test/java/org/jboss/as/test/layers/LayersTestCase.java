28a39d2b3de1a43fa3d976ff31f77670b38ad35c
JF Denise <jdenise@redhat.com>
2022-06-03 15:58:47 +0200

    Fix for WFLY-16863, Express default standalone configurations with Galleon layers

--- testsuite/layers/src/test/java/org/jboss/as/test/layers/LayersTestCase.java
+++ testsuite/layers/src/test/java/org/jboss/as/test/layers/LayersTestCase.java
@@ -293,10 +293,12 @@ public class LayersTestCase {
     }
 
     public static String root;
+    public static String defaultConfigsRoot;
 
     @BeforeClass
     public static void setUp() {
         root = System.getProperty("layers.install.root");
+        defaultConfigsRoot = System.getProperty("std.default.install.root");
     }
 
     @AfterClass
@@ -304,6 +306,10 @@ public class LayersTestCase {
         Boolean delete = Boolean.getBoolean("layers.delete.installations");
         if(delete) {
             File[] installations = new File(root).listFiles(File::isDirectory);
+            for(File f : installations) {
+                LayersTest.recursiveDelete(f.toPath());
+            }
+            installations = new File(defaultConfigsRoot).listFiles(File::isDirectory);
             for(File f : installations) {
                 LayersTest.recursiveDelete(f.toPath());
             }
@@ -320,5 +326,10 @@ public class LayersTestCase {
     public void checkBannedModules() throws Exception {
         final HashMap<String, String> results = LayersTest.checkBannedModules(root, BANNED_MODULES_CONF);
         Assert.assertTrue("The following banned modules were provisioned " + results.toString(), results.isEmpty());
+    }
+
+    @Test
+    public void testDefaultConfigs() throws Exception {
+        LayersTest.testExecution(defaultConfigsRoot);
     }
 }
