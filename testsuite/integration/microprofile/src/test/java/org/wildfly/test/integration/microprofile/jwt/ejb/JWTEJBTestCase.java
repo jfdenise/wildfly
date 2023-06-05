2241c21fdd2568ba0cbe704c2a5188cf3363532a
Kabir Khan <kkhan@redhat.com>
2023-06-05 13:00:21 +0100

    Fix tests to not use paths relative to project directory

--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/jwt/ejb/JWTEJBTestCase.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/jwt/ejb/JWTEJBTestCase.java
@@ -22,13 +22,6 @@
 
 package org.wildfly.test.integration.microprofile.jwt.ejb;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.wildfly.test.microprofile.jwt.TokenUtil.generateJWT;
-
-import java.io.File;
-import java.net.URL;
-
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -41,10 +34,17 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.asset.FileAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.test.integration.microprofile.jwt.BaseJWTCase;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.wildfly.test.microprofile.jwt.TokenUtil.generateJWT;
 
 /**
  * A test case for an Jakarta Enterprise Beans endpoint secured using the MP-JWT mechanism and invoking a
@@ -64,15 +64,11 @@ public class JWTEJBTestCase {
                 .add(EmptyAsset.INSTANCE, "WEB-INF/beans.xml")
                 .addClasses(JWTEJBTestCase.class)
                 .addClasses(App.class, BeanEndPoint.class, TargetBean.class)
-                .addAsManifestResource(new FileAsset(
-                        new File("src/test/resources/jwt/microprofile-config.properties")),
-                        "microprofile-config.properties")
-                .addAsManifestResource(
-                        new FileAsset(new File("src/test/resources/jwt/public.pem")),
-                        "public.pem");
+                .addAsManifestResource(BaseJWTCase.class.getPackage(), "microprofile-config.properties", "microprofile-config.properties")
+                .addAsManifestResource(BaseJWTCase.class.getPackage(), "public.pem", "public.pem");
     }
 
-    private static final String KEY_LOCATION = "src/test/resources/jwt/private.pem";
+    private static final URL KEY_LOCATION = BaseJWTCase.class.getResource("private.pem");
 
     private static final String ROOT_PATH = "/rest/Sample/";
     private static final String SUBSCRIPTION = "subscription";
@@ -93,7 +89,7 @@ public class JWTEJBTestCase {
 
     @Test
     public void testAuthorized() throws Exception {
-        String jwtToken = generateJWT(KEY_LOCATION, PRINCIPAL_NAME, DATE, ECHOER_GROUP, SUBSCRIBER_GROUP);
+        String jwtToken = generateJWT(Paths.get(KEY_LOCATION.toURI()).toAbsolutePath().toString(), PRINCIPAL_NAME, DATE, ECHOER_GROUP, SUBSCRIBER_GROUP);
 
         HttpGet httpGet = new HttpGet(deploymentUrl.toString() + ROOT_PATH + SUBSCRIPTION);
         httpGet.addHeader(AUTHORIZATION, BEARER + " " + jwtToken);
